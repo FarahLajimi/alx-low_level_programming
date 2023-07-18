d1a9cd1513be00431e65f5bdfddc0bdeed8a8503
farah <farah.lajimi1@gmail.com>
2023-07-18 15:06:50 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,14 +6,14 @@
 */
 
 void print_alphabet(void)
-
 {
-        int ch;
+	int i, ch;
 
-       for (i = 0; i < 10; i++)
-       {
-	       for (ch = 'a'; ch <= 'z'; ch++)
-                _putchar(ch);
-	       _putchar('\n');
-       }
+	for (i = 0; i < 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+
 }
